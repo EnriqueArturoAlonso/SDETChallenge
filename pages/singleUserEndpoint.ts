04085071a0f0ac7481f5ce63d099492233c63937
Enrique Alonso <earturo@Enriques-MacBook-Pro.local>
2026-04-28 10:30:42 -0600

    File cleanup

--- pages/singleUserEndpoint.ts
+++ pages/singleUserEndpoint.ts
@@ -12,13 +12,11 @@ export class SingleUserEndpoint extends baseEndpoint {
 
   async updateUser(user:User):Promise<APIResponse>{
     const response = await this.put('/users/'+user.email, user);
-    console.log(response);
     return response;
   }
 
   async deleteUser(user:User):Promise<APIResponse>{
     const response = await this.delete('/users/'+user.email);
-    console.log(response);
     return response;
   }
 };
